c63d5828263c147f5b4b6e7c2e9ccfb5531f6bf9
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2018-12-13 07:00:56 +0100

    Change to Google docstring style, add shapes to docs

--- model/lstm2d_cell.py
+++ model/lstm2d_cell.py
@@ -2,32 +2,45 @@ import torch.nn as nn
 
 
 class LSTM2dCell(nn.Module):
+    """
+    A 2d-LSTM Cell that computes it's hidden state and cell state based on
+        - an input x
+        - the previous horizontal hidden and cell state
+        - the previous vertical hidden and cell state
+
+    Args:
+        input_dim: the input dimension (i.e. second dimension of x)
+        state_dim: dimension of the hidden and cell state of this LSTM unit
+    """
+
     def __init__(self, input_dim, state_dim):
         super(LSTM2dCell, self).__init__()
         self.input_dim = input_dim
         self.state_dim = state_dim
 
         # input to state
-        self.W = nn.Linear(self.input_dim, self.state_dim * 5)
+        self.W_x = nn.Linear(self.input_dim, self.state_dim * 5)
         # previous horizontal hidden state to state
-        self.U = nn.Linear(self.state_dim, self.state_dim * 5)
+        self.W_hor = nn.Linear(self.state_dim, self.state_dim * 5)
         # previous vertical hidden state to state
-        self.V = nn.Linear(self.state_dim, self.state_dim * 5)
+        self.W_ver = nn.Linear(self.state_dim, self.state_dim * 5)
 
-    def forward(self, xj, s_horizontal, s_vertical, c_horizontal, c_vertical):
+    def forward(self, x, s_prev_hor, s_prev_ver, c_prev_hor, c_prev_ver):
         """
         Forward pass of the 2d-LSTM Cell at horizontal step j and vertical step i (to compute c_ji and s_ji)
 
-        :param xj: input at horizontal step j
-        :param s_horizontal: hidden state of cell at previous horizontal step j-1, same vertical step i
-        :param s_vertical: hidden state of cell at previous vertical step i-1, same horizontal step j
-        :param c_horizontal: cell state of cell at previous horizontal step j-1, same vertical step i
-        :param c_vertical: cell state of cell at previous vertical step i-1, same horizontal step j
-        :return:
-            c_ji: next cell state
-            s_ji: next hidden state
+        Args:
+            x: (batch x input_dim) input at horizontal step j
+            s_prev_hor: (batch x state_dim) hidden state of cell at previous horizontal step j-1, same vertical step i
+            s_prev_ver: (batch x state_dim) hidden state of cell at previous vertical step i-1, same horizontal step j
+            c_prev_hor: (batch x state_dim) cell state of cell at previous horizontal step j-1, same vertical step i
+            c_prev_ver: (batch x state_dim) cell state of cell at previous vertical step i-1, same horizontal step j
+
+        Returns:
+            c: (batch x state_dim) next cell state (c_ji)
+            s: (batch x state_dim) next hidden state (s_ji)
         """
-        pre_activation = self.W(xj) + self.U(s_horizontal) + self.V(s_vertical)
+        pre_activation = self.W_x(x) + self.W_hor(s_prev_hor) + self.W_ver(s_prev_ver)
         gates = pre_activation[:, :4*self.state_dim].sigmoid()
 
         # retrieve input, forget, output and lambda gate from gates
@@ -37,10 +50,10 @@ class LSTM2dCell(nn.Module):
         l = gates[:, 3*self.state_dim:4*self.state_dim]
 
         c_candidate = pre_activation[:, 4*self.state_dim:].tanh()
-        c_ji = f * (l * c_horizontal + (1 - l) * c_vertical) + c_candidate * i
-        s_ji = c_ji.tanh() * o
+        c = f * (l * c_prev_hor + (1 - l) * c_prev_ver) + c_candidate * i
+        s = c.tanh() * o
 
-        return c_ji, s_ji
+        return c, s
 
 
 
