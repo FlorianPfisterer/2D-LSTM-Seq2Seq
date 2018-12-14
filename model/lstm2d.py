25d1063f56ba475694834f7ef908c160eb9fb97e
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2018-12-14 11:20:33 +0100

    Start 2D-LSTM forward pass at training time, add code to determine input/output ranges for diagonal

--- model/lstm2d.py
+++ model/lstm2d.py
@@ -6,6 +6,7 @@ from model.lstm2d_cell import LSTM2dCell
 class LSTM2d(nn.Module):
     """
     2D-LSTM sequence-to-sequence (2D-seq2seq) model.
+
     Based on the paper
         "Towards two-dimensional sequence to sequence model in neural machine translation."
         Bahar, Parnia, Christopher Brix, and Hermann Ney
@@ -53,7 +54,6 @@ class LSTM2d(nn.Module):
         Returns:
             y_hat: (max_output_len x batch x vocab_size)
                 predicted output sequence (softmax distribution over vocab_size)
-
         """
         h = self.__encoder_lstm(x)
 
@@ -63,16 +63,43 @@ class LSTM2d(nn.Module):
             return self.__inference_forward(h)
 
     def __training_forward(self, h, y):
-        raise NotImplementedError()
+        """
+        Optimized implementation of the 2D-LSTM forward pass at training time, where the correct tokens y are known in
+        advance.
+        Processes the input in a linear-time diagonal-wise fashion, as described in the paper
+            Handwriting Recognition with Large Multidimensional Long Short-Term Memory Recurrent Neural Networks
+            by Voigtlaender et. al.
+
+        Args:
+            h: (max_input_len x batch x 2*encoder_state_dim) hidden states of bidirectional encoder LSTM
+            y: (max_output_len x batch) correct output tokens (indices in range [0, vocab_size))
+
+        Returns:
+            y_hat: (max_output_len x batch x vocab_size)
+                predicted output sequence (softmax distribution over vocab_size)
+        """
+        # obtain embedding representations for the correct tokens
+        y_emb = self.embedding.forward(y)   # (max_output_len x batch x embed_dim)
+
+        min_len = min(self.max_output_len, self.max_input_len)
+        max_len = max(self.max_output_len, self.max_input_len)
+
+        # number of cells computed concurrently, currently (length of the diagonal)
+        width = 1           # will be 1 2 3 ... min_len ... min_len ... min_len-1 min_len-2 ... 3 2 1
+
+        for diagonal_num in range(min_len + max_len - 1):
+            (h_from, h_to), (y_from, y_to) = self.__calculate_input_ranges(diagonal_num)
+            # TODO WIP
 
     def __inference_forward(self, h):
         """
-        Naive O(n^2) implementation of the 2d-LSTM forward pass at inference time.
+        Naive O(n^2) implementation of the 2D-LSTM forward pass at inference time.
         Args:
             h: (max_input_len x batch x 2*encoder_state_dim) hidden states of bidirectional encoder LSTM
 
         Returns:
-            y: (max_output_len x batch x vocab_size) output sequence (softmax distribution over vocab_size)
+            y_hat: (max_output_len x batch x vocab_size)
+                predicted output sequence (softmax distribution over vocab_size)
         """
         batch_size = h.size()[1]
         y_i = torch.zeros(batch_size, self.embed_dim)    # TODO <start> token?
@@ -82,7 +109,7 @@ class LSTM2d(nn.Module):
         c_prev_i = torch.zeros(self.max_input_len, batch_size, self.state_dim_2d)
 
         # result tensor
-        y = torch.empty(self.max_output_len, batch_size, self.vocab_size)
+        y_hat = torch.empty(self.max_output_len, batch_size, self.vocab_size)
 
         # go through each decoder output step
         for i in range(self.max_output_len):
@@ -104,12 +131,12 @@ class LSTM2d(nn.Module):
             s_Ji = s_prev_hor   # final hidden state at this vertical step i
             y_pred_i = self.logits.forward(s_prev_hor)
             y_pred_i = self.softmax.forward(y_pred_i)
-            y[i, :, :] = y_pred_i
+            y_hat[i, :, :] = y_pred_i
 
             # next generated token embedding (TODO beam seach?)
             y_i = self.embedding.forward(torch.argmax(y_pred_i, dim=1))
 
-        return y
+        return y_hat
 
     def __encoder_lstm(self, x):
         """
@@ -125,4 +152,46 @@ class LSTM2d(nn.Module):
         h, _ = self.encoder.forward(embedded_x)     # (max_input_len x batch x 2*encoder_state_dim)
 
         return h
+
+    def __calculate_input_ranges(self, diagonal_num: int):
+        """
+        Calculates the ranges for horizontal (y) and vertical (h) inputs based on the number of the diagonal.
+
+        Args:
+            diagonal_num: the number of the diagonal, in range [0, max_input_len + max_output_len - 1)
+
+        Returns:
+            a tuple of two tuples:
+                input_range: the range of vertical input values (h) to consider for the current diagonal
+                output_range: the range of horizontal output values (y) to consider for the current diagonal
+
+            the two ranges always have the same length, which is between 1 and min(max_input_len, max_output_len)
+        """
+        min_len = min(self.max_output_len, self.max_input_len)
+        max_len = max(self.max_output_len, self.max_input_len)
+        assert 0 <= diagonal_num < min_len + max_len
+
+        if diagonal_num < min_len:
+            max_range = (0, diagonal_num + 1)
+            min_range = max_range
+        elif diagonal_num < max_len:
+            max_range = (diagonal_num - (min_len - 1), diagonal_num + 1)
+            min_range = (0, min_len)
+        else:
+            max_range = (diagonal_num - (min_len - 1), max_len)
+            min_range = (diagonal_num - (max_len - 1), min_len)
+
+        assert (max_range[1] - max_range[0]) == (min_range[1] - min_range[0])
+        assert max_len >= max_range[1] > max_range[0] >= 0
+        assert min_len >= min_range[1] > min_range[0] >= 0
+
+        # determine which one is for the input and which one for the output
+        if min_len == self.max_output_len:  # the input (vertical) is shorter or of equal length to the output
+            input_range = min_range
+            output_range = max_range
+        else:                               # the output (horizontal) is shorter or of equal length to the input
+            input_range = max_range
+            output_range = min_range
+
+        return input_range, output_range
 
